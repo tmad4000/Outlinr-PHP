7da91b93a448b4eda01ee8cfeaccb7ad59ef90c5
tmad4000 <dev.jacobcole@gmail.com>
2014-01-10 14:33:22 -0800

    default root pw as '' on localhost; gitignore

--- public_html/inc/mysql.inc.php
+++ public_html/inc/mysql.inc.php
@@ -9,6 +9,9 @@ if(LOCAL) {
 	define('DB_NAME', 'ideaoverflow2_ma');
 	define('DB_USER', 'root');
 	define('DB_PASS', 'root');
+
+	
+	
 } 
 else {
 
@@ -24,5 +27,9 @@ define('IDEAS_TBL', 'post_ideas_mitsugg');
 define('LINKS_TBL', 'links');
 define('IDEAMAPS_TBL', 'ideamaps');
 
-$MYSQLI_LINK = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
+
+$MYSQLI_LINK = @mysqli_connect(DB_HOST, DB_USER, DB_PASS);
+if(!$MYSQLI_LINK)
+	$MYSQLI_LINK = mysqli_connect(DB_HOST, DB_USER, '');
+
 mysqli_select_db($MYSQLI_LINK, DB_NAME);
