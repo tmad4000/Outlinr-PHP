a9b0e837054be95d35b41545d9590e7a3a7916df
tmad4000 <dev.jacobcole@gmail.com>
2014-01-19 06:04:21 +0000

    actually added grand vision links

--- public_html/inc/nav.inc.php
+++ public_html/inc/nav.inc.php
@@ -38,9 +38,11 @@ $r = mysqli_fetch_assoc($result)
          
         </ul>
         <p class="navbar-text pull-right"><a href="https://workflowy.com/shared/20888d4e-26e0-2c25-556a-24685029e818/">&nbsp;&nbsp;</a></p>
+         <p class="navbar-text pull-right">&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://ideaoverflowideplan.tk/" target="_blank">Grand Vision</a></p>
 <!--        <p class="navbar-text pull-right">Logged in as <a href="https://workflowy.com/shared/20888d4e-26e0-2c25-556a-24685029e818/">username</a></p>-->
         <p class="navbar-text pull-right">
-        <!--<input type="text" id="usremail" placeholder="(optional) Email"><input type="text" id="usrname" placeholder="(optional) Name">--><input type="text" id="usrhandle" placeholder="username"></p>
+        <!--<input type="text" id="usremail" placeholder="(optional) Email"><input type="text" id="usrname" placeholder="(optional) Name">-->
+        <input type="text" id="usrhandle" class="usr" placeholder="username"></p>
       </div><!--/.nav-collapse -->
 
     </div>
