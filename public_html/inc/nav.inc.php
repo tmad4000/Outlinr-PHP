f1c126725db66612d54341a996f27e4e8e55cb37
tmad4000 <dev.jacobcole@gmail.com>
2014-05-20 01:22:32 +0100

    upgraded navigation bar and style
    contains #hack

--- public_html/inc/nav.inc.php
+++ public_html/inc/nav.inc.php
@@ -8,11 +8,24 @@ $getmapid=intval((mysqli_real_escape_string($MYSQLI_LINK, htmlspecialchars(trim(
 $query = "SELECT * FROM ideamaps WHERE mapid={$getmapid}";
 //echo $query;
 $result = mysqli_query($MYSQLI_LINK, $query) or die("SELECT Error: " . mysqli_error($MYSQLI_LINK));
-$r = mysqli_fetch_assoc($result)
+$r = mysqli_fetch_assoc($result);
+
+$pagefilename=basename($_SERVER['PHP_SELF']);
+
+$pagesdict=array(
+  "index.1.7_suggestionbox_ideamaps.php"=>"ideamaps",
+  "index.1.7_suggestionbox.php"=>"list",
+  "index.1.7_suggestionbox_graph.php"=>"graph",
+  "index.1.7_suggestionbox_people.php"=>"people",
+  );
+
+  $page=$pagesdict[$pagefilename];
+;
 
 ?>
 
 <div class="row">
+
     <nav class="navbar navbar-default navbar-static-top" role="navigation">
         <div class="container-fluid">
           <!-- Brand and toggle get grouped for better mobile display -->
@@ -32,26 +45,35 @@ $r = mysqli_fetch_assoc($result)
               ?>
 
               <?= strpos($_SERVER['PHP_SELF'],"index.1.7_suggestionbox_ideamaps.php") ? '' : $r['mapname'] ?></div> 
-              
-            <span class="navbar-text">
-            
-            <a href="index.1.7_suggestionbox.php?mapid=<?= $getmapid ?>">Ideas (List)</a>
-            <a href="index.1.7_suggestionbox_graph.php?mapid=<?= $getmapid ?>">Ideas (Graph)</a>
-            <a href="index.1.7_suggestionbox_people.php?mapid=<?= $getmapid ?>">People</a>
-            
-            </span>
+<!--               <ul class="nav nav-tabs">
+  <li <?= $page=="list"?'class="active"':"" ?> ><a href="index.1.7_suggestionbox.php?mapid=<?= $getmapid ?>">Ideas List</a></li>
+  <li <?= $page=="graph"?'class="active"':"" ?> ><a href="index.1.7_suggestionbox_graph.php?mapid=<?= $getmapid ?>">Ideas Graph</a></li>
+  <li <?= $page=="people"?'class="active"':"" ?> ><a href="index.1.7_suggestionbox_people.php?mapid=<?= $getmapid ?>">People</a></li>
+</ul> -->
+
+            <ul class="nav navbar-nav">
+        <li <?= $page=="list"?'class="active"':"" ?> ><a href="index.1.7_suggestionbox.php?mapid=<?= $getmapid ?>">List View</a></li>
+  <li <?= $page=="graph"?'class="active"':"" ?> ><a href="index.1.7_suggestionbox_graph.php?mapid=<?= $getmapid ?>">Graph View</a></li>
+  <li <?= $page=="people"?'class="active"':"" ?> ><a href="index.1.7_suggestionbox_people.php?mapid=<?= $getmapid ?>">People</a></li>
+  
+        
+      </ul>
+           
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" style="float:right" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
 
+
+
+
               <li><a href="index.1.7_suggestionbox_ideamaps.php">+ Make an idea box</a></li>
               <input type="hidden" value="<?= $getmapid ?>" id="mapidform" />
             </ul>
             <form class="navbar-form nav navbar-nav navbar-right" role="search">
               <div class="form-group">
-                <span style="font-size:16pt;">~</span><input type="text" id="usrhandle" class="form-control" placeholder="Username">
+                <span style="font-size:12pt;">~</span><input type="text" id="usrhandle" class="form-control" placeholder="Username">
               </div>
             </form>
           </div><!-- /.navbar-collapse -->
