ae3208def435a00c50c34c9f318f1b459b2160b8
tmad4000 <dev.jacobcole@gmail.com>
2014-05-20 01:38:32 +0100

    fixed double text

--- public_html/index.1.7_suggestionbox.php
+++ public_html/index.1.7_suggestionbox.php
@@ -157,7 +157,7 @@ require_once('../config.inc.php');
           <div class="col-sm-12">
             <form id="postform">
               <div class="input-append">
-                <textarea class="span12" placeholder="Type an idea, suggestion, or goal for <?= strpos($_SERVER['PHP_SELF'],"index.1.7_suggestionbox_ideamaps.php") ? '' : $r['mapname'] ?> <?= strpos($_SERVER['PHP_SELF'],"index.1.7_suggestionbox_ideamaps.php") ? '' : $r['mapname'] ?>" id="newpost" ></textarea>
+                <textarea class="span12" placeholder="Type an idea, suggestion, or goal for <?= strpos($_SERVER['PHP_SELF'],"index.1.7_suggestionbox_ideamaps.php") ? '' : $r['mapname'] ?>" id="newpost" ></textarea>
               </div>
 <!--               <div class='related-ideas-all'>
                 <ul class='related-ideas'>
