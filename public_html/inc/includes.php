66f2aeed88b6095eb06d07b7105a4ea9a87ebb5c
David Furlong <dvfurlong@googlemail.com>
2014-05-24 20:22:36 +0100

    changes

--- public_html/inc/includes.php
+++ public_html/inc/includes.php
@@ -7,6 +7,7 @@
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/util.js"></script>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link href="stylesheet/less" type="text/css" href="styles/ui.less" />
 <link rel="stylesheet/less" type="text/css" href="styles/main.less" />
 <script type="text/javascript" src="js/lib/typeahead.bundle.js"></script>
 <script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.3.0/handlebars.js'></script>
