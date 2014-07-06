1f64f186f71b1ae1e08942c94c96f0e3913f2e7c
David Furlong <dvfurlong@googlemail.com>
2014-07-06 11:45:57 -0400

    rollback get or make post

--- public_html/ajax/get_or_make_post_ideamaps.php
+++ public_html/ajax/get_or_make_post_ideamaps.php
@@ -2,17 +2,16 @@
 require_once('../../config.inc.php');
 require_once(PATH.PATH_SEP.'inc/mysql.inc.php');
 
-$maptitle = mysqli_real_escape_string($MYSQLI_LINK, htmlspecialchars($_REQUEST['name']));
-$mapadminemail = mysqli_real_escape_string($MYSQLI_LINK, htmlspecialchars($_REQUEST['email']));
-$mapadminpassword = mysqli_real_escape_string($MYSQLI_LINK, htmlspecialchars($_REQUEST['password']));
+$maptitle = mysqli_real_escape_string($MYSQLI_LINK, htmlspecialchars($_REQUEST['newpost']));
 $time = time();
 
 $ideamapstbl = IDEAMAPS_TBL;
 
 if (!empty($maptitle)) {
-    $query = "INSERT INTO $ideamapstbl (`mapid`, `mapname`, `email`, `password`) VALUES ('', '$maptitle', '$email', '$password')";
+	$tmptitle=substr($body,0,80);
+    $query = "INSERT INTO $ideamapstbl (`mapid`, `mapname`) VALUES ('', '$maptitle')";
     $result = mysqli_query($MYSQLI_LINK, $query) or die("INSERT Error: " . mysqli_error($MYSQLI_LINK));
-    print mysql_insert_id();
+//	print $query;
 }
 
 $query = "SELECT * FROM $ideamapstbl ORDER BY mapid";
