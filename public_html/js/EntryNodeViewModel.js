41f7a8e12a1e2a488338a3d251a72d54df0c18e8
David Furlong <dvfurlong@googlemail.com>
2014-08-12 20:21:40 -0400

    popovers working once more

--- public_html/js/EntryNodeViewModel.js
+++ public_html/js/EntryNodeViewModel.js
@@ -167,7 +167,7 @@ function EntryNodeViewModel(entryNodeModel) {
 			else {
 				upvoter='<td class="votes" -idea-id="'+this.entryNodeModel.pid+'"><span class="vote"><i class="ion-ios7-arrow-up"></i></span><span class="votes" >'+this.entryNodeModel.upvotes+'</span></td>';
 			}
-			var starCol = "<td class='star-idea'><span class='big-status status'><i class='ion-record status sc"+this.entryNodeModel.status +"'></i></td>";
+			var starCol = "<td class='star-idea'><span class='big-status status'>"+'<a href="#" rel="popover" data-content="'+progEntry +this.entryNodeModel.metric+'" data-original-title="'+statusTable[this.entryNodeModel.status]+'">'+"<i class='ion-record status sc"+this.entryNodeModel.status +"'></i></a></span></td>";
 
 			
 
